b356ce53acd0084116903370dcf752dcd3c9fb1c
Sofia de Oliveira Marques <sofiaomarques26@gmail.com>
2026-07-09 16:25:13 -0300

    Remove fundo

--- features/cores.py
+++ features/cores.py
@@ -1,29 +1,48 @@
 import numpy as np
 from PIL import Image
 from sklearn.cluster import KMeans
+from rembg import remove
+
+def remover_fundo(imagem):
+    imagem_sem_fundo = remove(imagem)
+    # converte para RGB ignorando pixels transparentes
+    imagem_rgba = np.array(imagem_sem_fundo)
+    
+    # pega só pixels não transparentes (alpha > 0)
+    pixels_validos = imagem_rgba[imagem_rgba[:,:,3] > 0][:,:3]
+    return pixels_validos
 
 def extrair_cores(caminho_imagem):
+    # abre a imagem
     imagem = Image.open(caminho_imagem).convert("RGB")
     imagem = imagem.resize((100, 100))
-    
-    pixels = np.array(imagem).reshape(-1, 3).astype(float)
-    
+
+    # remove o fundo e pega só pixels da roupa
+    try:
+        pixels = remover_fundo(imagem)
+    except:
+        # se falhar usa todos os pixels
+        pixels = np.array(imagem).reshape(-1, 3).astype(float)
+
+    pixels = pixels.astype(float)
+
+    # K-Means pra encontrar as 2 cores principais
     kmeans = KMeans(n_clusters=2, random_state=42, n_init=10)
     kmeans.fit(pixels)
-    
+
     cores = kmeans.cluster_centers_.astype(int).tolist()
-    
+
     cor_principal  = cores[0]
     cor_secundaria = cores[1]
-    
+
     brilho = sum(cor_principal) / 3
     if brilho > 160:
-        tonalidade = 1
+        tonalidade = 0  
     elif brilho > 60:
-        tonalidade = 2
+        tonalidade = 1  
     else:
-        tonalidade = 3
-    
+        tonalidade = 2  
+
     return {
         "cor_principal":  cor_principal,
         "cor_secundaria": cor_secundaria,
